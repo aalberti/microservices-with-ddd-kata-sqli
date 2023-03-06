64bf99469384f17cea86031c7c0d2c1bcbb3ae0f
hbentalha <adm-azr-hicham.bentalha@chanel.com>
2023-03-06 14:32:43 +0100

    Refacto

--- bounded-contexts/java/src/main/java/p1/myshop/order/ShippingWeightController.java
+++ bounded-contexts/java/src/main/java/p1/myshop/order/ShippingWeightController.java
@@ -1,16 +1,14 @@
 package p1.myshop.order;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import p1.myshop.catalog.CatalogService;
-import p1.myshop.catalog.Item;
-import p1.myshop.catalog.ItemId;
 import p1.myshop.shipping.DeliveryService;
 import p1.myshop.shoppingcart.Cart;
-import p1.myshop.shoppingcart.CartItem;
+import p1.myshop.shoppingcart.ShoppingCarService;
+
+import java.util.stream.Collectors;
 
 @RestController
 public class ShippingWeightController {
@@ -20,15 +18,11 @@ public class ShippingWeightController {
 
 	@Autowired
 	CatalogService catalogService;
-
+	@Autowired
+	ShoppingCarService shoppingCarService;
 
 	@GetMapping("/shippingweight")
 	public Object shippingweight(/* @RequestParam */ Cart cart) {
-		List<Item> items = cart.items().stream()
-				.map(CartItem::productId)
-				.map(productId -> ItemId.from(productId.toString()))
-				.map(catalogService::loadItem).collect(Collectors.toList());
-
-		return delivery.calculateOrderWeight(items);
+       return delivery.calculateOrderWeight(shoppingCarService.toItemIds(cart).stream().map(catalogService::loadItem).collect(Collectors.toList()));
 	}
 }
--- bounded-contexts/java/src/main/java/p1/myshop/shoppingcart/ShoppingCarService.java
+++ bounded-contexts/java/src/main/java/p1/myshop/shoppingcart/ShoppingCarService.java
@@ -1,5 +1,18 @@
 package p1.myshop.shoppingcart;
 
-public class ShoppingCarService {
+import org.springframework.beans.factory.annotation.Autowired;
+import p1.myshop.catalog.CatalogService;
+import p1.myshop.catalog.ItemId;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ShoppingCarService {
+   @Autowired
+    CatalogService catalogService;
+    public List<ItemId> toItemIds(Cart cart) {
+       return cart.items().stream()
+                .map(CartItem::productId)
+                .map(productId -> ItemId.from(productId.toString())).collect(Collectors.toList());
+    }
 }
